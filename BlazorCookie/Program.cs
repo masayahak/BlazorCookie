8ceb0c6f1829900602be23f801bffe41ec2eee21
masayahak <masayahak@outlook.jp>
2024-12-04 16:49:01 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorCookie/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using BlazorCookie.Components;
+using BlazorCookie.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// ----------- Cookie認証 --------------------------------
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+    });
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthenticationService>();
+// -------------------------------------------------------
+
+// 詳細なエラーを有効化
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true;
+});
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+// 認証・認可
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ BlazorCookie/Services/AuthenticationService.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace BlazorCookie.Services
+{
+    public class AuthenticationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthenticationService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public async Task<bool> LoginAsync(LoginUser loginUser)
+        {
+
+            var claims = new List<Claim>();
+
+            if (loginUser.UserName == "admin@test.com" && loginUser.Password == "test")
+            {
+                claims.Add(new Claim(ClaimTypes.Name, loginUser.UserName));
+                claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            }
+            else if (loginUser.UserName == "user@test.com" && loginUser.Password == "test")
+            {
+                claims.Add(new Claim(ClaimTypes.Name, loginUser.UserName));
+                claims.Add(new Claim(ClaimTypes.Role, "User"));
+            }
+            else
+            {
+                return false;
+            }
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext == null)
+                throw new InvalidOperationException("HttpContext is not available.");
+
+            await httpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(identity));
+            return true;
+
+        }
+        public async Task LogoutAsync()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext == null)
+                throw new InvalidOperationException("HttpContext is not available.");
+
+            await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+    public class LoginUser
+    {
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+
+}
+
